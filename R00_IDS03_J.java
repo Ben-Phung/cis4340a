5d2c48ca712f601abe598a8333f985b9151f9993
Ben-Phung <90236372+Ben-Phung@users.noreply.github.com>
2024-03-02 22:14:00 -0500

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -3,7 +3,4 @@ if (loginSuccessful){
 } else {
   logger.severe("User login failed for: " + sanitizeUser(username));
 }
-public String sanitizeUser(String username) {
-  return Pattern.matches("[A-Za-z0-9_]+", username))
-      ? username : "unauthorized user";
-}
+
