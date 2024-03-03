b897048d7fcc4c689eeac006fb2a203578b96bd6
Ben-Phung <90236372+Ben-Phung@users.noreply.github.com>
2024-03-02 22:16:30 -0500

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,6 +1,8 @@
-if (loginSuccessful){
- logger.severe("User login succeeded for: " + sanitizeUser(username));
-} else {
- logger.severe("User login failed for: " + sanitizeUser(username));
+public class R00_IDS03_J {
+    public static void main(String [] args){
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + sanitizeUser(username));
+    } else {
+      logger.severe("User login failed for: " + sanitizeUser(username));
+    }
 }
-
