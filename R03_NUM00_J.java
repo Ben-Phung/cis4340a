34cc6603bffdda949fa6b80236c0201c1db23841
Ben-Phung <90236372+Ben-Phung@users.noreply.github.com>
2024-03-02 22:28:07 -0500

    Update R03_NUM00_J.java

--- R03_NUM00_J.java
+++ R03_NUM00_J.java
@@ -1,4 +1,3 @@
 public static int multAccum(int oldAcc, int newVal, int scale) {
-  // May result in overflow
-  return oldAcc + (newVal * scale);
+  return safeAdd(oldAcc, safeMultiply(newVal, scale));
 }
