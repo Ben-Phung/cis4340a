9f702199d175cc9308ec81ee908a8097c7c112e4
Ben-Phung <90236372+Ben-Phung@users.noreply.github.com>
2024-03-02 22:13:45 -0500

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,9 +1,6 @@
-public class R00_IDS03_J {
-    public static void main(String [] args){
-        if (loginSuccessful) {
+if (loginSuccessful) {
             logger.severe("User login succeeded for: " + username);
           } else {
             logger.severe("User login failed for: " + username);
           }
-    }
-}
+
