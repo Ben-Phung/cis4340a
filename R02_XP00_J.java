bca799e4c87ec1f2090ea1639bfbea2db9b27e78
Ben-Phung <90236372+Ben-Phung@users.noreply.github.com>
2024-03-02 22:10:28 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,11 +1,6 @@
-public class R02_XP00_J {
-
-    public void deleteFile(){
+public void deleteFile(){
  
-        File someFile = new File("someFileName.txt");
-        // Do something with someFile
-        someFile.delete();
-       
-      }
-
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    someFile.delete();
 }
